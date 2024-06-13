35fce989d8d8bf0107e39d0a2605c7067dc0cc38
easyhyun00 <leejihyun0324@gmail.com>
2024-06-13 15:58:24 +0900

    💄 Style: 로그인, 회원가입 스타일 수정

--- components/common/AuthContainer/index.tsx
+++ components/common/AuthContainer/index.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import style from './style.module.scss';
 import PandaLogo from '@/public/svgs/logo-bg.svg';
 import Link from 'next/link';
+import useIsMobile from '@/hooks/useIsMobile';
 
 interface AuthContainerProps {
   children: React.ReactNode;
 }
 
 const AuthContainer = ({ children }: AuthContainerProps) => {
+  const isMobile = useIsMobile();
+
   return (
     <div className={style.container}>
       <header className={style.header}>
         <Link href="/">
-          <PandaLogo width={380} height={120} />
+          <PandaLogo
+            width={isMobile ? 198 : 396}
+            height={isMobile ? 66 : 122}
+          />
         </Link>
       </header>
       {children}
