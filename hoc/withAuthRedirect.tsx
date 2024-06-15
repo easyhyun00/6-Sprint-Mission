ebd2c20bab2ff8c498f34413c7244539c9d7519b
easyhyun00 <leejihyun0324@gmail.com>
2024-06-16 01:26:23 +0900

    ♻️ Refactor: withAuthRedirect 에 로딩 스피너 추가

--- hoc/withAuthRedirect.tsx
+++ hoc/withAuthRedirect.tsx
@@ -1,17 +1,38 @@
-import React, { useEffect, ComponentType, FC } from 'react';
+import React, { useEffect, ComponentType, FC, useState } from 'react';
 import { useRouter } from 'next/router';
 import { STORAGE_KEYS } from '@/constants/storageKey';
+import LoadingSpinner from '@/public/svgs/spinner.svg';
 
 const withAuthRedirect = <P extends {}>(WrappedComponent: ComponentType<P>) => {
   const WithAuthRedirect: FC<P> = (props) => {
     const router = useRouter();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       const accessToken = localStorage.getItem(STORAGE_KEYS.accessToken);
       if (accessToken) {
         router.push('/');
+      } else {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
       }
     }, [router]);
+
+    if (loading) {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <LoadingSpinner width="200px" height="200px" />
+        </div>
+      );
+    }
 
     return <WrappedComponent {...props} />;
   };
